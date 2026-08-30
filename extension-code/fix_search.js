159555e1b22732840009b0a0e96742bfe484a45a
Vishwaroop <vishwaroopg8@gmail.com>
2026-08-30 13:06:41 +0530

    Clean up

--- extension-code/fix_search.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('sidepanel.js', 'utf8');
-
-js = js.replace(/const response = await fetch\(https:\/\/www\.screener\.in\/api\/company\/search\/\?q=\\$\\{encodeURIComponent\(query\)\\}\);\n\s*if \(\!response\.ok\) return;\n\s*const results = await response\.json\(\);/,
-  \const results = await new Promise(resolve => {
-            chrome.runtime.sendMessage({ type: 'SEARCH_COMPANY', query: query }, resolve);
-          });\);
-
-fs.writeFileSync('sidepanel.js', js, 'utf8');
