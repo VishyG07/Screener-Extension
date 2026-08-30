5509467f12a522f3640b2c6ff29d7dad0ee25760
Vishwaroop <vishwaroopg8@gmail.com>
2026-08-30 13:01:58 +0530

    Clean up merge scripts

--- extension-code/merge_clean.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('sidepanel.js', 'utf8');
-
-js = js.replace(/\[tabSearch, tabWatchlist, tabNews\]/g, "[tabSearch, tabNews]");
-js = js.replace(/if \(activeTab === tabWatchlist\) renderWatchlist\(\);/g, "if (activeTab === tabSearch) renderWatchlist();");
-js = js.replace(/if \(tabWatchlist\.classList\.contains\('active'\)\) renderWatchlist\(\);/g, "if (tabSearch.classList.contains('active')) renderWatchlist();");
-
-fs.writeFileSync('sidepanel.js', js, 'utf8');
--- extension-code/merge_clean_2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('sidepanel.js', 'utf8');
-
-js = js.replace(/\[viewSearch, viewWatchlist, viewNews\]/g, "[viewSearch, viewNews]");
-
-fs.writeFileSync('sidepanel.js', js, 'utf8');
